aa085a63eca61b9c46cf1b137248464c7d9854fc
Paulo Gabriel <paulogabriel042@gmail.com>
2021-09-21 15:33:44 -0400

    JogoDaVelha-v1.1

---
+++ main.py
@@ -0,0 +1,23 @@
+from jogo_da_velha import criarBoard, printBoard, getInputValido, \
+                        verificarMovimento, fazMovimento,verificarGanhador
+
+jogador = 0 # Jogador 1
+board = criarBoard()
+ganhador = verificarGanhador(board)
+
+while (not ganhador):
+   printBoard(board)
+   i = getInputValido('Digite a linha: ')
+   j = getInputValido('Digite a coluna: ')
+
+   if (verificarMovimento(board, i, j)):
+      fazMovimento(board, i, j, jogador)
+      jogador = (jogador + 1) % 2
+   else:
+      print('A posição informada já está ocupada!!')
+
+   ganhador = verificarGanhador(board)
+print("==" * 20)
+printBoard(board)
+print("Ganhador = ", ganhador)
+print("==" * 20)
